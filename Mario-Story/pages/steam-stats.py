1937bf4a34b50a239a3d6cad7d18f94c8bcfa35b
Kevin <kevindsouza476@gmail.com>
2023-10-26 19:15:28 -0400

    Filtered to 36 games that seem to have a consistent playerbase, at least holding 8 hours of playtime in two weeks.

--- Mario-Story/pages/steam-stats.py
+++ Mario-Story/pages/steam-stats.py
@@ -21,7 +21,7 @@ st.markdown(
 
 columns = list(df.select_dtypes(include='number'))
 y_cond = st.selectbox('Pick column to display', columns)
-st.bar_chart(df,x="Name",y=y_cond)
+#st.bar_chart(df,x="Name",y=y_cond)
 
 st.markdown(
     """
@@ -38,16 +38,34 @@ st.write(df_CCU.shape)
 st.markdown(
     """
     So, there are 112 games above 15k, so lets further refine our search.
-    In this next scatter plot, the FARTHER they are from the Y axis, the more average playtime
+    Now we can try to see games the game's average playtime over two weeks, in minutes, to see if the games are still "alive"
     """
 )
+#Check for the most 2 week average/median playtime, and then see release date for anomalies
+scat_PT = alt.Chart(df_CCU).mark_circle(size=60).encode(
+    x='Average playtime two weeks',
+    y='Median playtime two weeks',
+    color='Average playtime two weeks',
+    tooltip =['Name','Release date','Average playtime two weeks','Median playtime two weeks'],
+).properties(
+        width = 1000,
+        height = 500 
+    )
 
-alt_chart = alt.Chart(df_CCU).mark_circle(size=60).encode(
-    x='Median playtime forever',
-    y='Average playtime forever',
-    tooltip = ['Name']
-).interactive()
-st.altair_chart(alt_chart)
+#want to ask, median exposes if we have outliers? Someone playing 10x the average, will skew the average, but the median will just get the middle of all data.
 
+st.altair_chart(scat_PT)
+select_cond = df_CCU['Average playtime two weeks'] > 500
+df_outliers=df_CCU[select_cond]
+select_cond =  df_CCU['Median playtime two weeks'] > 500
+df_outliers = df_outliers[select_cond]
+st.write(df_outliers)
+st.write(df_outliers.shape)
 
+st.markdown(
+    """
+    So there are 36 games that fit our outliers. 
+    Slowly through filtering, we see games that were MASSIVE "Hogwarts Legacy with Peak CCU of 872000 people", aren't even holding above 8 hours of playtime in two weeks
+    """
 
+)
