bddc106ff3f892b606dd95a64e4de67362bdae8a
peter-3140FA2022 <po5302006@hotmail.com>
2023-11-10 09:02:11 -0500

    reorganized customized_plot.py in a cleaner format

--- pages/Customized_plot.py
+++ pages/Customized_plot.py
@@ -19,67 +19,64 @@ df = pd.read_csv('./data/join_02.csv')
 # Format preparation
 df['date'] = pd.to_datetime(df['date'])
 df['release_date'] = pd.to_datetime(df['release_date'])
-df = df[df['avg_peak_perc']!='NaN%']
+df['avg_peak_perc'] = df['avg_peak_perc'].str.rstrip('%').astype('float') 
 df = df.dropna()
 
 # Header
 st.header("👋")
-st.title("Customized Plot")
+st.title("Customized Plot on :blue[General Features]")
 
-##### Filter #####
-
-# Featuer for x axis
-
+##### FILTER #####
+# Featuer for both axis
 features = ['avg', 'gain', 'peak', 'avg_peak_perc']
-order_bool = st.toggle(label='Ascending')
-y = st.selectbox("Select a Feature", features)
-# left_col, right_col = st.columns(2)
-# with right_col: y = st.selectbox("Select a Feature", features)
-# with left_col: order_bool = st.toggle(label='Ascending')
+genres = features
 
-order='Worst' if order_bool else 'Highest'
+left_col, right_col = st.columns(2)
+order = st.toggle(label='Find the Worst Games', value=False)        # descending order toggle switch
+y = st.selectbox("Select a Feature", features)                      # feature select box
+order_name='Worst' if order else 'Highest'                          # string formating
 y_name = y.replace('_', ' ').title()
 
-# Data - sorting
-gb = df[['gamename', 'date', y]].sort_values(by=y, ascending=order_bool).reset_index()
-top_games = gb.gamename.unique()[0:5]
+# Data - sorting and filtering
+df_ax = df[['gamename', 'date', y]].sort_values(by=y, ascending=order).reset_index()    # Data - Plot 1
+top_games = df_ax.gamename.unique()[0:5]
+df_bx = df[['gamename', 'date']+features].sort_values(by=y, ascending=order).reset_index()      # Data - Plot 2
 
-# Data Frame
-title = f"1.1 Dataset Sorted by :blue[{y_name}]:"
+# Dataframe preview
+title = f"1.1 Dataset of Games Sorted by :blue[{y_name}]:"
 st.subheader(title)
-st.dataframe(gb)
+st.dataframe(df_ax)
 
-title = f"1.2 Five Games with the {order} Monthly :blue[{y_name}]:"
+title = f"1.2 Five Games with the Overall :red[{order_name}] Monthly :blue[{y_name}]:"
 st.subheader(title)
 st.write(top_games)
 
-# Plot 1 - markdown
-st.markdown(
-    """
-    ***
-    ## 2.0 Customized Plot
-    """
-)
 
-# Plot 1 Multiselect box
+
+##### PLOT 1 #####
+# Plot 1 - markdown
+st.markdown("""***""")
+title = f"1.3 Games with the Overall :red[{order_name}] :blue[{y_name}]"
+st.subheader(title)
+
+# Plot 1 - select box
 options = top_games
 selected_options = st.multiselect('Select Video Games', options)
 
 # Plot 1
-selected_names = ','.join(selected_options)
-title = f"Monthly :blue[{y_name}] of {selected_names} Over Time"
-st.write(title)
+title_names = ','.join(selected_options)
+plot_title = f"Monthly {y_name} of {title_names} Over Time"
+gb = df_ax.sort_values(by='date')
+gb_list = {game: gb[gb["gamename"] == game] for game in selected_options}
 
-gb2 = gb.sort_values(by='date')
-gb_list = {game: gb2[gb2["gamename"] == game] for game in selected_options}
-
-
-fig = go.Figure()
-# fig.update_layout(title=title)
-fig.update_layout(
-    xaxis_title='Date',
-    yaxis_title=y_name,
+fig_1 = go.Figure()
+fig_1.update_layout(
+    title = plot_title, 
+    xaxis_title = 'Date',
+    yaxis_title = y_name,
 )
-for game, gb2 in gb_list.items():
-    fig = fig.add_trace(go.Scatter(x=gb2["date"], y=gb2[y], name=game, mode='lines'))
-st.plotly_chart(fig)
+for game, gb in gb_list.items():
+    fig_1 = fig_1.add_trace(go.Scatter(x=gb["date"], y=gb[y], name=game, mode='lines'))
+st.plotly_chart(fig_1)
+
+
